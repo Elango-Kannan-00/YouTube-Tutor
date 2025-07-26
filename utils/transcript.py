a9790d2bafb8127bd8778f1662d5be9582f79228
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-26 22:52:50 +0530

    deleted utils files

--- utils/transcript.py
+++
@@ -1,6 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-
-def get_transcript(video_url):
-    video_id = video_url.split("v=")[-1].split("&")[0]
-    transcript = YouTubeTranscriptApi.get_transcript(video_id)
-    return " ".join([entry['text'] for entry in transcript])
