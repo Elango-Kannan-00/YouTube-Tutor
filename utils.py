6029df6375ac1a40c48f7e0279b1e3d86d7d565a
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-07-27 23:47:47 +0530

    utils file

---
+++ utils.py
@@ -0,0 +1,11 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+def get_transcript(video_url):
+    video_id = video_url.split("v=")[-1].split("&")[0]
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    full_text = " ".join([entry["text"] for entry in transcript])
+    return full_text
+
+def chunk_text(text, chunk_size=500):
+    words = text.split()
+    return [" ".join(words[i:i+chunk_size]) for i in range(0, len(words), chunk_size)]
